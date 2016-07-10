a63112fdd886739e87bf7c9eccf8cd285f489502
Jim Dixon <jddixon@gmail.com>
2016-07-10 15:06:20 -0700

    add ghpDoc/{css,img}; update .dvcz

--- src/java/org/xlattice/util/Version.java
+++ src/java/org/xlattice/util/Version.java
@@ -10,7 +10,7 @@ public class Version extends org.xlattice.Version {
         super("org.xlattice.util", // package
             0,                       // major
             3,                       // minor
-            16,                       // decimal
+            17,                       // decimal
             0);                      // build
     }
 };
